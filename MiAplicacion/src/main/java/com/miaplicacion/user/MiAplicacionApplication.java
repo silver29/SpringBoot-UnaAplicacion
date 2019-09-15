da7ee8fa0a0db43d132867f969e8d068549ec16b
silver29 <becquerk29@gmail.com>
2019-09-15 10:45:26 -0500

    commit inicial con eclipse.Close #1

---
+++ MiAplicacion/src/main/java/com/miaplicacion/user/MiAplicacionApplication.java
@@ -0,0 +1,13 @@
+package com.miaplicacion.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiAplicacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiAplicacionApplication.class, args);
+	}
+
+}
